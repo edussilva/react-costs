b7a8f8b088f7190c69471c4c07ce2cd6e951432e
edussilva <edussilva@live.com>
2023-09-07 17:13:29 -0300

    Adicionada as paginas iniciais

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Contact from "./components/pages/Contact";
+import Company from "./components/pages/Company";
+import NewProject from "./components/pages/NewProject";
+
+
 function App() {
   return (
     <div className="App">
-      <p>Costs</p>
+      
+      <Router>
+        <div>
+          <Link to="/">Home</Link>
+          <Link to="/company">Company</Link>
+          <Link to="/contact">Contact</Link>
+          <Link to="/newproject">NewProject</Link>
+        </div>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/company' element={<Company />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/newproject' element={<NewProject />} />
+        </Routes>
+        <footer>Footer</footer>
+      </Router>
     </div>
   );
 }
