441eef292a810cad4a217a81d4201077bd8974bf
edussilva <edussilva@live.com>
2023-09-08 14:31:24 -0300

    Criação da navbar

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/pages/Home";
 import Contact from "./components/pages/Contact";
 import Company from "./components/pages/Company";
 import NewProject from "./components/pages/NewProject";
 import Container from "./components/layout/Container";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Projects from "./components/pages/Projects";
 
 function App() {
   return (
     <div className="App">
       
       <Router>
-        <div>
-          <Link to="/">Home</Link>
-          <Link to="/company">Company</Link>
-          <Link to="/contact">Contact</Link>
-          <Link to="/newproject">NewProject</Link>
-        </div>
+        <Navbar />
         <Container customClass="min-height">
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/company' element={<Company />} />
             <Route path='/contact' element={<Contact />} />
             <Route path='/newproject' element={<NewProject />} />
+            <Route path='/projects' element={<Projects />} />
           </Routes>
         </Container>
-        <footer>Footer</footer>
+        <Footer />
       </Router>
     </div>
   );
